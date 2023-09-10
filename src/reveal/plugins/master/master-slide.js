b24b1154d4d0107303dd5bd4263d724b0da45081
Christopher Keers <blizzardengle@users.noreply.github.com>
2023-09-10 00:13:16 -0600

    feat: multiple slide templates are allowed now

--- src/reveal/plugins/master/master-slide.js
+++ src/reveal/plugins/master/master-slide.js
@@ -16,24 +16,57 @@
  *     <p class="copyright">
  *         &copy; 2022&ndash;<script>document.write(new Date().getFullYear());</script> Caboodle Tech Inc.
  *     </p>
+ *     <!-- Element to use for background colors or images via custom css. You can also add elements inside. -->
+ *     <div class="background"></div>
  * </section>
  *
  * @returns Reveal style plugin function.
  */
 function MasterSlide() {
 
-    let masterHTML = '';
+    const masterSlides = {
+        __default: null
+    };
 
     /**
      * Finds all backgrounds for this presentation and adds the master slide content to them.
      *
      * @param {HTMLElement} parent The root element for this presentation.
      */
-    const addMaterTemplate = (parent) => {
+    const addMaterTemplates = (parent) => {
+        // First check all the slides for which template they should be using.
+        const masterMap = [];
+        const slides = parent.parentElement.querySelectorAll('.slides section');
+        for (let i = 0; i < slides.length; i++) {
+            const slide = slides[i];
+            // Everything starts as default.
+            let templateName = '__default';
+            if (slide.dataset.template) {
+                templateName = slide.dataset.template;
+            }
+            // If a stack is encountered and had as template apply it to all it's children.
+            if (slide.classList.contains('stack')) {
+                const children = slide.querySelectorAll('section');
+                children?.forEach(() => {
+                    masterMap[i] = templateName;
+                    i += 1;
+                });
+            }
+            // Record the template for this slide; or the last slide in a nested section.
+            masterMap[i] = templateName;
+        }
+
+        // Now apply the correct template to each slide.
         const backgrounds = parent.querySelectorAll('.slide-background .slide-background-content');
-        backgrounds.forEach((background) => {
+        backgrounds.forEach((background, i) => {
             // eslint-disable-next-line no-param-reassign
-            background.innerHTML = masterHTML;
+            background.innerHTML = masterSlides[masterMap[i]] || '';
+            // Add the template name as a class so users can style it easier.
+            if (masterMap[i] !== '__default') {
+                background.classList.add(masterMap[i]);
+            } else {
+                background.classList.add('defualt');
+            }
         });
     };
 
@@ -44,12 +77,20 @@ function MasterSlide() {
      */
     const initialize = (deck) => {
         // If no master slide was found return now.
-        const master = deck.querySelector('.master-slide');
-        if (!master) { return; }
+        const masters = deck.querySelectorAll('.slide-template');
+        if (!masters || masters.length < 1) { return; }
 
-        // Master slide found, record its content and then remove it from the page.
-        masterHTML = master.innerHTML;
-        master.remove();
+        // At least one master slide was found, record its content and then remove it from the page.
+        masters.forEach((master) => {
+            if (master.dataset.template) {
+                masterSlides[master.dataset.template] = master.innerHTML;
+            // eslint-disable-next-line no-underscore-dangle
+            } else if (!masterSlides.__default) {
+                // eslint-disable-next-line no-underscore-dangle
+                masterSlides.__default = master.innerHTML;
+            }
+            master.remove();
+        });
 
         // Create a new MutationObserver that waits until Reveal builds the slide backgrounds.
         const observer = new MutationObserver((mutationsList) => {
@@ -62,7 +103,7 @@ function MasterSlide() {
                         if (addedNode?.classList?.contains('backgrounds')) {
                             // The backgrounds have been added by Reveal, modify them now.
                             observer.disconnect();
-                            addMaterTemplate(addedNode);
+                            addMaterTemplates(addedNode);
                             return;
                         }
                     }
